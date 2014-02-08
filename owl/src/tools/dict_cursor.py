f899a332f55122bba574b62c2b95f858c7ca2828
dosumis <dosumis@gmail.com>
2014-02-08 21:18:44 +0000

    owl gen files now moved to separate repo.  started adding images for doc

--- owl/src/tools/dict_cursor.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env jython
-
-def dict_cursor(cursor):
-    description = [x[0] for x in cursor.description]
-    for row in cursor:
-        yield dict(zip(description, row))  # Not sure that yield is the best option here, as produces a 
