a4d3cce286e05c97f1e2353aedd8d71b1737c0c3
Robert Court <robbie.court@gmail.com>
2014-03-11 15:07:06 +0000

    replaced duplicate code
    now uses decompress script

--- deploy/Deploy-Dev-Server.sh
+++ deploy/Deploy-Dev-Server.sh
@@ -22,23 +22,7 @@ then
         echo 'Note: to manually modify, use instructions in filters folder, only if a change is required'
     fi
 
-    echo "Decompressing any *.gz or *.gz.part-?? data..."
-    for filename in find . -name '*.gz.part-aa'
-    do
-        mergedname=${filename/gz.part-aa/gz}
-        echo merging files to reconstruct ${mergedname}
-        cat ${filename/part-aa/part-??} > ${mergedname}
-        echo decompressing and removing gzip copy of ${mergedname} 
-        pigz -df ${mergedname}
-        echo completed deploying ${mergedname/.qz/}
-    done
-    for filename in find . -name '*.gz'
-    do
-        decomname=${filename/.gz/}
-        echo decompressing and keeping gzip copy of ${filename} 
-        pigz -dkf ${filename}
-        echo completed deploying ${decomname}
-    done   
+    deploy/decompress.sh
     
     echo "recording git branch and version details"
     git describe --long > revision
