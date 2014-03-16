2d0ad7ac4baa360ebf2ff5e4e85c8d53c79795b3
Robert Court <robbie.court@gmail.com>
2014-03-16 12:13:57 +0000

    Adding ontology server deployment

---
+++ deploy/start-Sandbox3-Server-Ont-Server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export VFBclasses=/usr/local/tomcat-6/webapps/vfb2sb/WEB-INF/classes
+export branch=Sandbox2-Server
+
+#start / force restart of ontology server 
+deploy/start-Ont-Server.bsh 
