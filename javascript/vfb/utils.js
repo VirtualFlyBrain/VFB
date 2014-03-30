cd379b0521b363f2fda5804ad68206d2b88212b0
Robert Court <robbie.court@gmail.com>
2014-03-30 16:46:45 -0700

    testing name passing

--- javascript/vfb/utils.js
+++ javascript/vfb/utils.js
@@ -39,7 +39,7 @@ function createAutocomplete() {
 			onSelect:function(elements, value){
 				var id_container = $("id_container"); 
 				id_container.set("value", value.id);
-				window.location = '/site/tools/anatomy_finder/index.htm?id=' + value.id; //redirects
+				window.location = '/site/tools/anatomy_finder/index.htm?id=' + value.id + ‘&name=‘ + encodeURIComponent($("scope").value); //redirects
 			},
 			filter: {
 				path: 'text', 
