60fe4fbcd701f9188f2b9c9bf7e9c193e22e4244
Robert Court <robbie.court@gmail.com>
2014-06-19 07:38:21 +0100

    corrected numbering

--- deploy/start-Sandbox3-Server-Ont-Server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export VFBclasses=/usr/local/tomcat-6/webapps/vfb2sb/WEB-INF/classes
-export branch=Sandbox2-Server
-
-#start / force restart of ontology server 
-deploy/start-Ont-Server.sh 
