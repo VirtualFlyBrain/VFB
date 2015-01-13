abeb6e6c3bc3df41bf77ff758318f7421b2248ba
Robbie1977 <robbie.court@gmail.com>
2015-01-13 16:53:05 +0000

    syntax fix

--- src/uk/ac/ed/vfb/dao/client_server/server_includes/AOwlResultParser.java
+++ src/uk/ac/ed/vfb/dao/client_server/server_includes/AOwlResultParser.java
@@ -38,7 +38,7 @@ public abstract class AOwlResultParser {
 	public OntBean getOntBeanForId(String id) {
 		try {
 			LOG.debug("OGW: " + this.ogw + " ID: " + OntBean.idAsOBO(id));
-			OWLEntity oo = (OWLEntity) this.ogw.getOWLObjectByIdentifier(OntBean.idAsOBO(id));
+			OWLEntity oo = (OWLEntity)this.ogw.getOWLObjectByIdentifier(OntBean.idAsOBO(id));
 			LOG.debug("OO: " + oo);
 			LOG.debug("from ontology" + this.ontology.toString());
 			return this.getOntBeanForEntity(oo);
