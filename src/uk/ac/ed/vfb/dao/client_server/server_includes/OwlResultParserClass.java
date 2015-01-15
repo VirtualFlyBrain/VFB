0e60f5b7661ccc9ae6cfd2f858d24da66edfc282
Robbie1977 <robbie.court@gmail.com>
2015-01-15 17:58:39 +0000

    syntax fix

--- src/uk/ac/ed/vfb/dao/client_server/server_includes/OwlResultParserClass.java
+++ src/uk/ac/ed/vfb/dao/client_server/server_includes/OwlResultParserClass.java
@@ -86,7 +86,7 @@ public class OwlResultParserClass extends AOwlResultParser {
 			}
 			// removing duplicates and adding full ref list
 			axioms = new ArrayList<String>(new HashSet<String>(axioms));
-			LOG.debug("Extended Refs: /n");
+			LOG.debug("Extended Refs: \n");
 			for (String axiom:axioms){
 				LOG.debug(axiom.toString() + "\n");
 			}
