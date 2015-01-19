b9d365946202590fcbb3f59a1a0bd5cacef9c385
Robbie1977 <robbie.court@gmail.com>
2015-01-19 14:06:10 +0000

    syntax fix

--- src/uk/ac/ed/vfb/dao/client_server/server_includes/OwlResultParserClass.java
+++ src/uk/ac/ed/vfb/dao/client_server/server_includes/OwlResultParserClass.java
@@ -70,7 +70,7 @@ public class OwlResultParserClass extends AOwlResultParser {
 			//LOG.debug("=========== synonyms ==============" + synonyms.size());
 			List<String> syns = new ArrayList<String>();
 			List<String> synXrefs = new ArrayList<String>();
-			Int refI = 1;
+			int refI = 1;
 			String refIs = "";
 			if (synonyms != null && !synonyms.isEmpty()) {
 				for (ISynonym syn:synonyms){
