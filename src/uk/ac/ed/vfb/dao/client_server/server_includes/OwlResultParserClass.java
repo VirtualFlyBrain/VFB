63420b3c7243c80dd65a7d58f1bf38dbad60841b
Robbie1977 <robbie.court@gmail.com>
2015-01-22 12:35:19 +0000

    syntax fix

--- src/uk/ac/ed/vfb/dao/client_server/server_includes/OwlResultParserClass.java
+++ src/uk/ac/ed/vfb/dao/client_server/server_includes/OwlResultParserClass.java
@@ -83,7 +83,6 @@ public class OwlResultParserClass extends AOwlResultParser {
 					refExists = false;
 					// adding synonyn xrefs to references list
 					if (syn.getXrefs()!=null) {
-						PubBean temp = new PubBean();
 						synXrefs = new ArrayList<String>(new HashSet<String>(syn.getXrefs()));
 						for (String synXref:synXrefs){
 							if (synXref!=null && !synXref.isEmpty()){
@@ -92,7 +91,7 @@ public class OwlResultParserClass extends AOwlResultParser {
 								if (refIs != ""){
 									refIs = refIs + "," + pbm.getBeanByRef(synXref).getShortref();
 								}else{
-									temp = PubBeanManager.getBeanByRef(synXref);
+									PubBean temp = new PubBean(PubBeanManager.getBeanByRef(synXref));
 									LOG.debug("Returned PubBean: " + temp);
 									refIs = temp.getShortref();
 									
