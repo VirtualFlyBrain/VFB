f9e5a18bd32140755951d6b003d1988f82548380
Robbie1977 <robbie.court@gmail.com>
2015-01-22 13:42:18 +0000

    syntax fix

--- src/uk/ac/ed/vfb/model/OntBean.java
+++ src/uk/ac/ed/vfb/model/OntBean.java
@@ -97,7 +97,7 @@ public class OntBean implements Comparable<Object>, Serializable{
 					try {
 						results.add(pbm.getBeanByRef(ref));
 					}catch (Exception ex) {
-						LOG.error("Cant find ref: " + ex.toString());
+						LOG.error("Cant find ref: " + ref;
 						if (ref!=null){
 							LOG.error("Defaulting on ref: " + ref );
 							results.add(new PubBean(ref,ref));
