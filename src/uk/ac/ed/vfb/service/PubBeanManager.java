d1a0361a76003d377d3d178a87aca4f032ba7da2
Robbie1977 <robbie.court@gmail.com>
2015-01-27 16:44:47 +0000

    resolving single pubbean

--- src/uk/ac/ed/vfb/service/PubBeanManager.java
+++ src/uk/ac/ed/vfb/service/PubBeanManager.java
@@ -36,9 +36,7 @@ public class PubBeanManager {
 				String[] parts = ref.split(":");
 				LOG.debug("FlyBase ref: " + parts[1]);
 				LOG.debug("dao: " + dao);
-				List<PubBean> temp = dao.getByRef(parts[1]);
-				LOG.debug("Found: " + temp);
-				bean = temp.get(0);
+				bean = dao.getByRef(parts[1]);
 				LOG.debug("Returned FlyBase bean: " + bean.toString());
 				return bean;
 			}
