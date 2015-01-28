c76c6f4e5ba8beb9a796aa54c3d67d740ac57b9a
Robbie1977 <robbie.court@gmail.com>
2015-01-28 13:33:38 +0000

    syntax fix

--- src/uk/ac/ed/vfb/dao/db/PubDAO.java
+++ src/uk/ac/ed/vfb/dao/db/PubDAO.java
@@ -43,7 +43,7 @@ public class PubDAO extends AQueryDAO {
 	}
 	
 	public PubBean getByRef(String ref) {
-		jdbcTemplate jdbc = this.jdbcTemplate();
+		JdbcTemplate jdbc = this.jdbcTemplate();
 		LOG.debug("MiniRef for ref: " + ref);
 		String query = this.getQueryForName("pubminirefbyref").replace("XXX", ref);
 		LOG.debug("MiniRef by ref query: " + query);
