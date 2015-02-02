b1acf1882e370e5098b1f902a538312cd4c40a8f
Robbie1977 <robbie.court@gmail.com>
2015-02-02 23:11:37 +0000

    flagging non curators

--- src/uk/ac/ed/vfb/model/PubBean.java
+++ src/uk/ac/ed/vfb/model/PubBean.java
@@ -92,6 +92,9 @@ public class PubBean {
 		String result = id;
 		if (id.contains("FBC:")){
 			result = id.replace("FBC:", "FlyBase Curator [").replace("-", " and ").replace("gg","Gary Grumbling").replace("VH","Volker Hartenstein").replace("MMC","Marta Mesquita da Costa").replace("AJ","Arnim Jenett").replace("ds555","David Osumi-Sutherland").replace("DS","David Osumi-Sutherland").replace("MA","Michael Ashburner").replace("SR","Simon Reeve").replace("SPR","Simon Reeve").replace("DOS","David Osumi-Sutherland") + "]";
+			if (id.contains("VH") || id.contains("AJ") || id.contains("GJ")){
+				result = result.replace("Curator","Consultant");
+			}
 			return result;
 		}
 		if (id.contains("FlyBrain_NDB:")){
