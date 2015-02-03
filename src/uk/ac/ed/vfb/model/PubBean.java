beeaeae9e7b234dea6417329fb0bb63d2ce6a83c
Robbie1977 <robbie.court@gmail.com>
2015-02-03 00:04:20 +0000

    allowing for consultant refs

--- src/uk/ac/ed/vfb/model/PubBean.java
+++ src/uk/ac/ed/vfb/model/PubBean.java
@@ -72,7 +72,7 @@ public class PubBean {
 			if (id.contains("MMC")){
 				return "http://orcid.org/0000-0001-5948-3092";
 			}
-			return "http://www.pubfacts.com/author/" + miniref.replace("FlyBase Curator [","").replace("]","").replace(" ","+");
+			return "http://www.pubfacts.com/author/" + miniref.replace("FlyBase Curator [","").replace("]","").replace("FlyBase Consultant [","").replace(" ","+");
 		}
 		if (id.contains("ISBN:")){
 			return "https://www.google.com/search?q=" + id.replace("-","");
