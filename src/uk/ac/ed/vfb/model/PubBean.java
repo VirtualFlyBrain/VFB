2a5aba2c6d43c625b3edbf3cd90d56ac2ea2218f
Robbie1977 <robbie.court@gmail.com>
2015-02-21 03:05:35 +0000

    hiding consultant names until approved.
    Resolves #380

--- src/uk/ac/ed/vfb/model/PubBean.java
+++ src/uk/ac/ed/vfb/model/PubBean.java
@@ -138,6 +138,10 @@ public class PubBean {
 			if (id.contains("FlyBase") || id.contains("auto_generated_definition")){
 				return "http://flybase.org";
 			}
+			// TBR: to handle block consultant links.
+			if (miniref.contains("FlyBase"){ 
+				return "http://flybase.org";
+			}
 			return "http://www.pubfacts.com/author/" + miniref.replace("FlyBase Curator [","").replace("]","").replace("FlyBase Consultant [","").replace(" ","+");
 		}
 		if (id.contains("ISBN:")){
@@ -172,6 +176,7 @@ public class PubBean {
 			result = id.replace("FBC:", "FlyBase Curator [").replace("-", " and ").replace("gg","Gary Grumbling").replace("VH","Volker Hartenstein").replace("MMC","Marta Mesquita da Costa").replace("AJ","Arnim Jenett").replace("ds555","David Osumi-Sutherland").replace("DS","David Osumi-Sutherland").replace("MA","Michael Ashburner").replace("SR","Simon Reeve").replace("SPR","Simon Reeve").replace("DOS","David Osumi-Sutherland").replace("KI","Kei Ito") + "]";
 			if (id.contains("VH") || id.contains("AJ") || id.contains("GJ") || id.contains("KI")){
 				result = result.replace("Curator","Consultant");
+				result = "FlyBase" // TBR: once the Consultants are approved.
 			}
 			return result;
 		}
