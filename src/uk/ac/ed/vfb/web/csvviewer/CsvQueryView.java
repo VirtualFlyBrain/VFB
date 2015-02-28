a0b543c13335458bce99ce609e9d0ce1d6e96ad8
Robbie1977 <robbie.court@gmail.com>
2015-02-28 14:27:50 +0000

    syntax fix

--- src/uk/ac/ed/vfb/web/csvviewer/CsvQueryView.java
+++ src/uk/ac/ed/vfb/web/csvviewer/CsvQueryView.java
@@ -31,7 +31,7 @@ public class CsvQueryView  extends CsvViewer {
 			}else{
 				link = "=HYPERLINK(\"http://www.virtualflybrain.org/site/tools/anatomy_finder/index.htm?id=" + curr.correctIdFormat() + "\")";
 			}
-			line = "\"" + curr.getName() + "\",\"" + curr.getDef() + "\",\"" + link + "\"\n";
+			line = "\"" + curr.getName() + "\",\"" + curr.getDef() + "\"," + link + "\n";
 			writer.write(line);
 		}
 	}
