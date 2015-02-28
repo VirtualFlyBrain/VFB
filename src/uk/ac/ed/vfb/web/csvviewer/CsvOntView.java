3917ec270f69b428290eda5f3eba272c984e6f14
Robbie1977 <robbie.court@gmail.com>
2015-02-28 14:22:17 +0000

    can't escape comma

--- src/uk/ac/ed/vfb/web/csvviewer/CsvOntView.java
+++ src/uk/ac/ed/vfb/web/csvviewer/CsvOntView.java
@@ -26,9 +26,9 @@ public class CsvOntView  extends CsvViewer {
 		while (it.hasNext()) {
 			OntBean curr = it.next();
 			if (curr.correctIdFormat().contains("VFB")){
-				link = "=HYPERLINK(\"http://www.virtualflybrain.org/" + curr.correctIdFormat() + "\",\"" + curr.correctIdFormat() + "\")";
+				link = "=HYPERLINK(\"http://www.virtualflybrain.org/" + curr.correctIdFormat() + "\")";
 			}else{
-				link = "=HYPERLINK(\"http://www.virtualflybrain.org/site/tools/anatomy_finder/index.htm?id=" + curr.correctIdFormat() + "\",\"" + curr.correctIdFormat() + "\")";
+				link = "=HYPERLINK(\"http://www.virtualflybrain.org/site/tools/anatomy_finder/index.htm?id=" + curr.correctIdFormat() + "\")";
 			}
 			line = "\"" + curr.getName() + "\",\"" + curr.getDef() + "\",\"" + link + "\"\n";
 			writer.write(line);
