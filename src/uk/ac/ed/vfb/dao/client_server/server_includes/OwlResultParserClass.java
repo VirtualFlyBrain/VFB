35a8ba48cbf445dcaedd9854e2aa63102181847e
Robbie1977 <robbie.court@gmail.com>
2015-03-05 19:58:49 +0000

    syntax fix

--- src/uk/ac/ed/vfb/dao/client_server/server_includes/OwlResultParserClass.java
+++ src/uk/ac/ed/vfb/dao/client_server/server_includes/OwlResultParserClass.java
@@ -128,7 +128,7 @@ public class OwlResultParserClass extends AOwlResultParser {
 			String relName = "";
 			OWLAnnotationProperty namePropery = ogw.getAnnotationProperty("name");
 			for (OWLSubClassOfAxiom rel:rels){
-				relI++
+				relI++;
 				//LOG.debug("rel" + relI + " : " + rel.toString() + " > " + "\n");
 				//LOG.debug("=========== rel props ==============");
 				Set<OWLObjectProperty> props = rel.getObjectPropertiesInSignature();
