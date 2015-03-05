030eb6186c03aa8d5dbb639424fba0734e477836
Robbie1977 <robbie.court@gmail.com>
2015-03-05 15:11:08 +0000

    Reducing delay time by looking for server ready

--- deploy/start-Ont-Server.sh
+++ deploy/start-Ont-Server.sh
@@ -21,7 +21,13 @@ then
   nohup nice /usr/lib/jvm/java-1.7.0-sun/bin/java -classpath ../lib/postgresql-9.0-801.jdbc3.jar:../lib/commons-pool.jar:../lib/commons-dbcp.jar:${path}/../lib/servlet-api.jar:../lib/spring.jar::../lib/commons-logging.jar:../lib/log4j.jar:../lib/Brain-1.5.1-jar-with-dependencies.jar:../lib/elk-owlapi.jar:../lib/owltools-oort-all.jar:. -Djava.rmi.server.codebase=file:$path -Dlog4j.configuration=./log4j.properties uk.ac.ed.vfb.dao.client_server.Server > ../../logs/ontServer.log 2>&1 &
 
 echo 'waiting for the ontology server to initialise...'
-sleep 2m
+MAX_WAIT=24
+until [ `cat logs/ontServer.log | grep "Wainting for a connection" | wc -l` -gt 0 ] || [ $MAX_WAIT -lt 1 ]; do
+   sleep 5s
+   let MAX_WAIT=MAX_WAIT-1
+done
+let MAX_WAIT=MAX_WAIT*5
+echo Ontology server restart took ${MAX_WAIT} Seconds
 om tomcat restart
 else
   echo "Must be run from specific server script"
