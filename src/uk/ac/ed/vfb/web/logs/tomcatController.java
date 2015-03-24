21f0748f8311d5d1f48c8b78902b2b9108495196
Rob Court <robbie.court@gmail.com>
2015-03-24 17:15:51 +0000

    syntax fix

--- src/uk/ac/ed/vfb/web/logs/tomcatController.java
+++ src/uk/ac/ed/vfb/web/logs/tomcatController.java
@@ -24,7 +24,6 @@ public class tomcatController implements Controller {
   public ModelAndView handleRequest(HttpServletRequest req, HttpServletResponse res) throws Exception {
     ModelAndView modelAndView = new ModelAndView("logs/tomcat");
     String logfile = "/disk/data/tomcat/fly/webapps/vfb/logs/tomcat.log";
-    List<String> data = new ArrayList<String>();
     try{
       LOG.debug("Loading info from meta file: " + getFileForId(getTemplateId()).replace("composite.wlz", "meta.json"));
       List<String> lines = Files.readAllLines(Paths.get(logfile), StandardCharsets.UTF_8);
