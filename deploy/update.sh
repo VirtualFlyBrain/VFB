1023d277edff9f39e53d8bcb0ed3b82cc006f2fe
Rob Court <robbie.court@gmail.com>
2015-03-26 23:03:03 +0000

    adding owl ind revision info

--- deploy/update.sh
+++ deploy/update.sh
@@ -24,13 +24,16 @@ then
         echo $branch > branch
         cp /disk/data/VFB/Chado/VFB_DB/current/revision flybase
         head -n 100 resources/fbbt-simple.owl | grep versionIRI | sed 's/^[^"]*"\([^"]*\)".*/\1/' > owldate
+        head -n 100 resources/fbbt_vfb_ind_pr_nr.owl | grep versionIRI | sed 's/^[^"]*"\([^"]*\)".*/\1/' > owlIndRev
         echo "which are:"
         cat branch
         cat revision
         echo "Flybase version:"
         cat flybase
-        echo "OWL date:"
+        echo "OWL ontology version:"
         cat owldate
+        echo "OWL individual version:"
+        cat owlIndRev
 
         echo "checking filters to use correct branch names"
         find filters/ -name 'Filt*Smudge.sed' | xargs sed -i -f filters/Local-General-Clean.sed
