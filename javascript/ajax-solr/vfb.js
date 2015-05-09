50bc331a48894622b6d0be4e8429da060f7367d5
Robert Court <robbie.court@gmail.com>
2015-05-09 15:51:14 +0100

    showing preliminary results

--- javascript/ajax-solr/vfb.js
+++ javascript/ajax-solr/vfb.js
@@ -6,20 +6,20 @@ var Manager;
     Manager = new AjaxSolr.Manager({
       solrUrl: 'http://blanik.inf.ed.ac.uk:8983/solr/ontology/'
     });
-    // Manager.addWidget(new AjaxSolr.ResultWidget({
-    //   id: 'result',
-    //   target: '#docs'
-    // }));
-    // Manager.addWidget(new AjaxSolr.PagerWidget({
-    //   id: 'pager',
-    //   target: '#pager',
-    //   prevLabel: '&lt;',
-    //   nextLabel: '&gt;',
-    //   innerWindow: 1,
-    //   renderHeader: function (perPage, offset, total) {
-    //     $('#pager-header').html($('<span></span>').text('displaying ' + Math.min(total, offset + 1) + ' to ' + Math.min(total, offset + perPage) + ' of ' + total));
-    //   }
-    // }));
+    Manager.addWidget(new AjaxSolr.ResultWidget({
+      id: 'result',
+      target: '#docs'
+    }));
+    Manager.addWidget(new AjaxSolr.PagerWidget({
+      id: 'pager',
+      target: '#pager',
+      prevLabel: '&lt;',
+      nextLabel: '&gt;',
+      innerWindow: 1,
+      renderHeader: function (perPage, offset, total) {
+        $('#pager-header').html($('<span></span>').text('displaying ' + Math.min(total, offset + 1) + ' to ' + Math.min(total, offset + perPage) + ' of ' + total));
+      }
+    }));
     // var fields = [ 'topics', 'organisations', 'exchanges' ];
     // for (var i = 0, l = fields.length; i < l; i++) {
     //   Manager.addWidget(new AjaxSolr.TagcloudWidget({
