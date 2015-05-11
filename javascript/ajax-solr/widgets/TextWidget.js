b5972b9565873325250ee0d61b6cd634760b7c9a
Robert Court <robbie.court@gmail.com>
2015-05-11 15:48:31 +0100

    test fix for iOS

--- javascript/ajax-solr/widgets/TextWidget.js
+++ javascript/ajax-solr/widgets/TextWidget.js
@@ -3,7 +3,7 @@
 AjaxSolr.TextWidget = AjaxSolr.AbstractTextWidget.extend({
   init: function () {
     var self = this;
-    $(this.target).find('input').bind('keydown', function(e) {
+    $(this.target).find('input').bind('keypress', function(e) {
       if (e.which == 13) {
         var value = $(this).val();
         if (value.contains(':')){
