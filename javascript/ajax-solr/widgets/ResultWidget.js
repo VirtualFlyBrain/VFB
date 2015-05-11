b38e3fc5325eb5a7e52fdf1913164e0d3a4ddf21
Robert Court <robbie.court@gmail.com>
2015-05-11 14:15:51 +0100

    syntax fix

--- javascript/ajax-solr/widgets/ResultWidget.js
+++ javascript/ajax-solr/widgets/ResultWidget.js
@@ -53,7 +53,8 @@ AjaxSolr.ResultWidget = AjaxSolr.AbstractWidget.extend({
     }
     if (this.manager.response.response.numFound == 1) { // only show results list if multiple results
       $('#anatomyDetails').load('/do/ont_bean.html?id=' + doc.short_form[0].replace(':','_'));
-      self.manager.store.get('q').val('*:*');
+      self.manager.store.remove('q');
+      self.manager.store.addByValue('q', '*:*');
       self.manager.store.remove('fq');
       $('#result-section').hide();
       self.doRequest();
