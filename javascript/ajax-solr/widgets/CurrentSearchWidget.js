09546ce50c83e54ff51bb6acae47862141668e1a
Robert Court <robbie.court@gmail.com>
2015-05-11 12:19:56 +0100

    syntax fix

--- javascript/ajax-solr/widgets/CurrentSearchWidget.js
+++ javascript/ajax-solr/widgets/CurrentSearchWidget.js
@@ -10,8 +10,7 @@ AjaxSolr.CurrentSearchWidget = AjaxSolr.AbstractWidget.extend({
     var q = this.manager.store.get('q').val();
     if (q != '*:*') {
       if (!q.contains(':')){ // force wildcard search by default
-        q = 'label_suggest:*' + q + '*';
-        this.manager.store.removeByValue('q', this.manager.store.get('q').val());
+        q = 'label:' + q;
         this.manager.store.addByValue('q', q);
       }
       links.push($('<a href="#"></a>').text('(x) ' + q).click(function () {
