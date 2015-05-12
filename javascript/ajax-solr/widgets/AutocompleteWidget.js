580f38d3480c6275ab9d9fbe4b026f5ca62538a7
Robert Court <robbie.court@gmail.com>
2015-05-12 09:32:02 +0100

    ensuring correct escaping

--- javascript/ajax-solr/widgets/AutocompleteWidget.js
+++ javascript/ajax-solr/widgets/AutocompleteWidget.js
@@ -83,7 +83,7 @@
       }
       var values = this.manager.store.values('fq');
       for (var i = 0; i < values.length; i++) {
-        params.push('fq=' + encodeURIComponent(values[i]));
+        params.push('fq=' + AjaxSolr.Parameter.escapeValue(values[i]));
       }
       params.push('q=' + this.manager.store.get('q').val());
       $.getJSON(this.manager.solrUrl + 'select?' + params.join('&') + '&wt=json&json.wrf=?', {}, callback);
