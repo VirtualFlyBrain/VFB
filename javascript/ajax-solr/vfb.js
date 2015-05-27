09f750af7c53df6a33fc2f3b916fec0fe49ba0a6
Robert Court <robbie.court@gmail.com>
2015-05-27 13:57:06 +0200

    switching to proxy solr

--- javascript/ajax-solr/vfb.js
+++ javascript/ajax-solr/vfb.js
@@ -4,7 +4,7 @@ var Manager;
 
   $(function () {
     Manager = new AjaxSolr.Manager({
-      solrUrl: 'http://blanik.inf.ed.ac.uk:8983/solr/ontology/'
+      solrUrl: 'http://blanik.inf.ed.ac.uk/search/'
     });
     Manager.addWidget(new AjaxSolr.ResultWidget({
       id: 'result',
