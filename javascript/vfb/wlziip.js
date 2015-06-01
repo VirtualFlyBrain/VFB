679a08856e371d6ba7c9d931bf26b349ece98b11
Robert Court <robbie.court@gmail.com>
2015-06-01 21:58:31 +0100

    testing passed value

--- javascript/vfb/wlziip.js
+++ javascript/vfb/wlziip.js
@@ -19,14 +19,16 @@
  }
 
  function fileFromId(id) {
-   id = id.replace(":","_");
-   var file = "";
-   if (id.indexOf("VFBt_") > -1){
-     file = id.replace("00000", "").replace("VFBt_","VFB/t/") + "/composite.wlz";
-   }else if (id.indexOf("VFBi_") > -1){
-     file = "VFB/i/" + id.substr(5,9) + "/" + id.substr(9,13) + "/volume.wlz";
-   }else if (id.indexOf("VFB_") > -1){
-     file = "VFB/i/" + id.substr(4,8) + "/" + id.substr(8,12) + "/volume.wlz";
+   if (id){
+     var file = "";
+     id = id.replace(":","_");
+     if (id.indexOf("VFBt_") > -1){
+       file = id.replace("00000", "").replace("VFBt_","VFB/t/") + "/composite.wlz";
+     }else if (id.indexOf("VFBi_") > -1){
+       file = "VFB/i/" + id.substr(5,9) + "/" + id.substr(9,13) + "/volume.wlz";
+     }else if (id.indexOf("VFB_") > -1){
+       file = "VFB/i/" + id.substr(4,8) + "/" + id.substr(8,12) + "/volume.wlz";
+     }
    }
    return file;
  }
