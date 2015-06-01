b800669a84840a04f37d12ff6e97efcd44394888
Robert Court <robbie.court@gmail.com>
2015-06-01 21:08:39 +0100

    location fix

--- javascript/vfb/wlziip.js
+++ javascript/vfb/wlziip.js
@@ -164,6 +164,6 @@ function initWlzDisplay(ids) {
    }else{
      colour = colours[index];
    }
-   var text = "/fcgi/wlziipsrv.fcgi?wlz=/disk/data/IMAGE_DATA/" + file + "&sel=" + layer.id.substr(9) + "," + colour + "&mod=" + current.mod + "&fxp=" + current.fxp + "&scl=" + current.scl + "&dst=" + current.dst + "&pit=" + current.pit + "&yaw=" + current.yaw + "&rol=" + current.rol + "&qlt=" + current.qlt + "&cvt=" + current.cvt;
+   var text = "/fcgi/wlziipsrv.fcgi?wlz=/disk/data/VFB/IMAGE_DATA/" + file + "&sel=" + layer.id.substr(9) + "," + colour + "&mod=" + current.mod + "&fxp=" + current.fxp + "&scl=" + current.scl + "&dst=" + current.dst + "&pit=" + current.pit + "&yaw=" + current.yaw + "&rol=" + current.rol + "&qlt=" + current.qlt + "&cvt=" + current.cvt;
    return text;
  }
