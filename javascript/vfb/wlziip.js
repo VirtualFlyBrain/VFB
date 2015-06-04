2736b049738de3a53949f3b101a03ec67ffd016b
Robert Court <robbie.court@gmail.com>
2015-06-04 09:44:43 +0100

    syntax fix

--- javascript/vfb/wlziip.js
+++ javascript/vfb/wlziip.js
@@ -184,7 +184,7 @@ function initWlzDisplay(ids) {
    });
    var slScale = $("#slider-scale").bootstrapSlider({reversed: false, min: 0.1, max: 5, value: 1, step: 0.1, focus: true, ticks: [1, 2], ticks_labels: ['1x', '2x'], ticks_snap_bounds: 0.3});
    slScale.on('slide', function(ev){
-     parent.$("body").data("current").scl = String($("#slider-scale").data('slider').value[0]);
+     parent.$("body").data("current").scl = String(ev.value);
      $("#slider-scaleSliderVal").text(ev.value);
    });
    slScale.on('slideStop', function(ev){
