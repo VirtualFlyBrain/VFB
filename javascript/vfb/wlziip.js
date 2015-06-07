3e2f25441b03eca496faf2d57ac12f6206a754fd
Robert Court <robbie.court@gmail.com>
2015-06-07 18:47:23 +0100

    starting collapsed

--- javascript/vfb/wlziip.js
+++ javascript/vfb/wlziip.js
@@ -408,13 +408,16 @@ function loadTemplateAnatomyTree() {
               var children = $(this).parent('li.parent_li').find(' > ul > li');
               if (children.is(":visible")) {
                   children.hide('fast');
-                  $(this).attr('title', 'Expand this branch').find(' > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');
+                  $(this).attr('title', 'Expand this branch').find(' > b').html('<span class="glyphicon glyphicon-plus-sign"></span>');
               } else {
                   children.show('fast');
-                  $(this).attr('title', 'Collapse this branch').find(' > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');
+                  $(this).attr('title', 'Collapse this branch').find(' > b').html('<span class="glyphicon glyphicon-minus-sign"></span>');
               }
               e.stopPropagation();
           });
+          // collapse all at start:
+          var children = $('.tree li.parent_li > span').parent('li.parent_li').find(' > ul > li');
+          children.hide('fast');
       });
      });
    }
