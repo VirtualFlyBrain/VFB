f4e97194d41aa5160d99626131289c846d785ee8
Robert Court <robbie.court@gmail.com>
2015-06-08 19:14:24 +0100

    swapping to green

--- javascript/vfb/stackManager.js
+++ javascript/vfb/stackManager.js
@@ -55,18 +55,18 @@ function generateAddButtons() {
       if ($(this).html() === ""){
         if (id.indexOf("FBbt_") > -1) {
           if (parent.$("body").data("available").indexOf(id) > -1) {
-            content += '<button type="button" class="btn btn-primary btn-xs" aria-label="Add to stack viewer" title="Add to stack viewer" onClick="';
+            content += '<button type="button" class="btn btn-success btn-xs" aria-label="Add to stack viewer" title="Add to stack viewer" onClick="';
             content += "addToStackData('" + id + "');updateStackData();if (typeof updateMenuData !== 'undefined' && $.isFunction(updateMenuData)) {updateMenuData();};if (typeof updateWlzDisplay !== 'undefined' && $.isFunction(updateWlzDisplay)) {updateWlzDisplay();};";
             content += '"><span style="border:none;" class="glyphicon glyphicon-paperclip"></span></button>';
           }
         }else if (id.indexOf("VFB") > -1) {
-          content += '<button type="button" class="btn btn-primary btn-xs" aria-label="Add to stack viewer" title="Add to stack viewer" onClick="';
+          content += '<button type="button" class="btn btn-success btn-xs" aria-label="Add to stack viewer" title="Add to stack viewer" onClick="';
           content += "addToStackData('" + id + "');updateStackData();if (typeof updateMenuData !== 'undefined' && $.isFunction(updateMenuData)) {updateMenuData();};if (typeof updateWlzDisplay !== 'undefined' && $.isFunction(updateWlzDisplay)) {updateWlzDisplay();};";
           content += '"><span style="border:none;" class="glyphicon glyphicon-paperclip"></span></button>';
         }
         $(this).html(content);
       }else if(JSON.stringify(parent.$("body").data(parent.$("body").data("current").template).selected).indexOf(id) > -1) {
-        content += '<button type="button" class="btn btn-primary btn-xs" aria-label="Remove from stack viewer" title="Remove" onClick="';
+        content += '<button type="button" class="btn btn-success btn-xs" aria-label="Remove from stack viewer" title="Remove" onClick="';
         content += "removeFromStackData('" + id + "');updateStackData();if (typeof updateMenuData !== 'undefined' && $.isFunction(updateMenuData)) {updateMenuData();};if (typeof updateWlzDisplay !== 'undefined' && $.isFunction(updateWlzDisplay)) {updateWlzDisplay();};";
         content += '"><span style="border:none;" class="glyphicon glyphicon-ok-circle"></span></button>';
         $(this).html(content);
