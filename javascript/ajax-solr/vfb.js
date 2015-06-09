7610b947982dbe08339d416f60c174f1a3f350ef
Robert Court <robbie.court@gmail.com>
2015-06-09 12:08:05 +0100

    filtering to VFB or FB entries

--- javascript/ajax-solr/vfb.js
+++ javascript/ajax-solr/vfb.js
@@ -48,7 +48,7 @@ var Manager;
       'facet.sort' : 'score',
       'facet.mincount': 1,
       'json.nl': 'map',
-      'fq' : 'short_form:*_*',
+      'fq' : 'short_form:VFB_* FBbt_*',
       'df' : 'label_suggest'
     };
     for (var name in params) {
