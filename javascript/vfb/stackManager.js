4176a9e9d7f9fec13986f0bf745dc2122929b32d
Robert Court <robbie.court@gmail.com>
2015-06-27 12:12:15 +0100

    marking resolved

--- javascript/vfb/stackManager.js
+++ javascript/vfb/stackManager.js
@@ -115,6 +115,7 @@ function generateAddButtons() {
     		  text += "parent.$('#query_builder').attr('src', '/do/query_builder.html?action=add&amp;rel=include&amp;fbId=" + $(this).data("id") + "');if (typeof openQueryTab !== 'undefined' && $.isFunction(openQueryTab)) {openQueryTab();};";
     		  text += '"><span style="border:none;" class="glyphicon glyphicon-tasks"></span></a>';
     		  $(this).html(text);
+          $(this).id="Resolved"+$(this).id;
         }
       });
   	}
