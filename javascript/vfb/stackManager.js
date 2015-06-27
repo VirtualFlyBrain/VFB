3cc842e2b9648d26fe8fbfa4a305d0a3f3a34794
Robert Court <robbie.court@gmail.com>
2015-06-27 12:08:24 +0100

    error handling

--- javascript/vfb/stackManager.js
+++ javascript/vfb/stackManager.js
@@ -110,10 +110,12 @@ function generateAddButtons() {
     $('#pageLoading').hide();
     if (window.location.pathname == "/site/stacks/index.htm") {
       $("#addToQuery").each(function(){
-  		  var text = '<a href="#" class="btn btn-xs btn-success" onclick="';
-  		  text += "parent.$('#query_builder').attr('src', '/do/query_builder.html?action=add&amp;rel=include&amp;fbId=" + $(this).data("id") + "');if (typeof openQueryTab !== 'undefined' && $.isFunction(openQueryTab)) {openQueryTab();};";
-  		  text += '"><span style="border:none;" class="glyphicon glyphicon-tasks"></span></a>';
-  		  $(this).html(text);
+        if ($(this).data("id") !=="undefined"){
+    		  var text = '<a href="#" class="btn btn-xs btn-success" onclick="';
+    		  text += "parent.$('#query_builder').attr('src', '/do/query_builder.html?action=add&amp;rel=include&amp;fbId=" + $(this).data("id") + "');if (typeof openQueryTab !== 'undefined' && $.isFunction(openQueryTab)) {openQueryTab();};";
+    		  text += '"><span style="border:none;" class="glyphicon glyphicon-tasks"></span></a>';
+    		  $(this).html(text);
+        }
       });
   	}
   }else{
