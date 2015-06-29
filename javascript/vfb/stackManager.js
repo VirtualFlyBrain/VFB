ce973e68f40ef944f2a1c4589375be08958d94b6
Robert Court <robbie.court@gmail.com>
2015-06-29 14:07:13 +0100

    set loading

--- javascript/vfb/stackManager.js
+++ javascript/vfb/stackManager.js
@@ -306,6 +306,7 @@ function alertMessage(message) {
 
 function openFullDetails(id) {
   if ($('#anatomyDetails')) {
+    $('#anatomyDetails').html('<img src="/javascript/ajax-solr/images/ajax-loader.gif" alt="loading...">');
     id = cleanIdforExt(id);
     if (id.indexOf("VFBt_") < 0 && id.indexOf("VFBd_") < 0){
       $('#anatomyDetails').load("/do/ont_bean.html?id=" + id);
