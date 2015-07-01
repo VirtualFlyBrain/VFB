d591b79e804f41f4bb295e5df504dba67f7e507f
Robert Court <robbie.court@gmail.com>
2015-07-01 01:03:18 +0100

    increasing to remove load on server

--- javascript/vfb/wlziip.js
+++ javascript/vfb/wlziip.js
@@ -111,7 +111,7 @@ function animateWlzDisplay(){
             }
           }
         }
-        if (window.reloadInterval > 2000) {
+        if (window.reloadInterval > 10000) {
           if (!updated && imageDist < 100) {
             var dist = current.dst;
             current.dst = dist + imageDist;
