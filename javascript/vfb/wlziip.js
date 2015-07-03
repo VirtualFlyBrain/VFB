bcbfc9274731abd26b3be392420f546ca271d7bf
Robert Court <robbie.court@gmail.com>
2015-07-03 04:13:56 +0100

    handling error on load

--- javascript/vfb/wlziip.js
+++ javascript/vfb/wlziip.js
@@ -46,7 +46,7 @@ function animateWlzDisplay(){
           if (selected[i].visible){
             if (!image[i]){
               image[i] = document.createElement('img');
-              image[i].onerror = "this.onerror=null;this.src='/img/blank.png';";
+              image[i].setAttribute('onerror', "this.onerror=null;this.src='/img/blank.png';");
               updated = true;
             }
             if (image[i].src.indexOf(generateWlzURL(i))<0){
