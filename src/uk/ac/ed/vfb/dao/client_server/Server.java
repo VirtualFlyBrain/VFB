c8c2e6c6a75528a6e40c14d13fbda2109ad82399
Robert Court <robbie.court@gmail.com>
2015-07-23 02:40:39 +0100

    syntax fix

--- src/uk/ac/ed/vfb/dao/client_server/Server.java
+++ src/uk/ac/ed/vfb/dao/client_server/Server.java
@@ -156,15 +156,15 @@ public class Server {
 			} catch (IOException ex) {
 				LOG.error("Ontology server IOException:");
 				ex.printStackTrace();
-				sendObjectToClient(null);
+				sendObjectToClient(this.results);
 			} catch (ClassNotFoundException ex) {
 				LOG.error("Ontology server ClassNotFoundException:");
 				ex.printStackTrace();
-				sendObjectToClient(null);
+				sendObjectToClient(this.results);
 			} catch (NullPointerException ex) {
 				LOG.error("Ontology server NullPointerException:");
         ex.printStackTrace();
-				sendObjectToClient(null);
+				sendObjectToClient(this.results);
       }
 		}
 	}
