285435d4e8240af1f040dcffeaf639261ae3223b
Robert Court <robbie.court@gmail.com>
2015-07-29 20:24:23 +0100

    activating orientation key

--- javascript/vfb/wlziip.js
+++ javascript/vfb/wlziip.js
@@ -200,6 +200,7 @@ function animateWlzDisplay(){
           }
         }
         addScale(50);
+        addOrientation();
         drawFeatures();
         if (window.reloadInterval > 999) {
           i++;
