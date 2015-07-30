58ee1919bbd604c633b8c57298e0a2dbfcd9bb49
Robert Court <robbie.court@gmail.com>
2015-07-31 00:01:43 +0100

    syntax fix

--- javascript/vfb/stackManager.js
+++ javascript/vfb/stackManager.js
@@ -45,10 +45,10 @@ function updateStackCounter() {
         if (stack[stack.current.template]){
           $(this).text(Object.keys(stack[stack.current.template].selected).length-1);
           if (typeof $.fn.dataTable !== 'undefined' && $.fn.dataTable.isDataTable('#displayed') && parseInt($(this).text()) !== (parseInt($('#displayed').dataTable().fnSettings().fnRecordsTotal()))-1) {
-            $(this).removeClass(label-success).addClass(label-danger);
+            $(this).removeClass('label-success').addClass('label-danger');
             $(this).attr('title', 'Too many items selected to save! Note: you can still work but items will not be saved; you can try clearing items in other templates to free space.');
           }else{
-            $(this).addClass(label-success).removeClass(label-danger);
+            $(this).addClass('label-success').removeClass('label-danger');
             $(this).attr('title', 'Number of items currently saved');
           }
         }else{
