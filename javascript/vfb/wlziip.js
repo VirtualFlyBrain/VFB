e3a9eaddb59fe555bedcdb8cdcb53a3213db08d1
Robert Court <robbie.court@gmail.com>
2015-07-30 22:56:16 +0100

    syntax fix

--- javascript/vfb/wlziip.js
+++ javascript/vfb/wlziip.js
@@ -887,9 +887,7 @@ function clearAllDisplayed() {
       $('#displayed').dataTable().fnDeleteRow(1, false );
       delete selected[i];
     }else{
-      if (i === 0){
-        selected[i].colour = "auto";
-      }
+      selected[i].colour = "auto";
     }
   }
   $('#displayed').dataTable().fnAdjustColumnSizing(false);
