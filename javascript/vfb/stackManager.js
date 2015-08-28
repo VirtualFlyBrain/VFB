aea0775605e251bb0360360dce3048a6f73e448d
Robert Court <robbie.court@gmail.com>
2015-08-28 10:18:17 +0100

    adding missing function

--- javascript/vfb/stackManager.js
+++ javascript/vfb/stackManager.js
@@ -525,6 +525,10 @@ function totalItemCount() {
   return i;
 }
 
+function escapeRegExp(string) {
+    return string.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+}
+
 function replaceAll(string, find, replace) {
   return string.replace(new RegExp(escapeRegExp(find), 'g'), replace);
 }
@@ -972,7 +976,7 @@ $('body').ready( function () {
             }else{
               resl = data.response.docs[i].short_form[1];
             }
-            if (i === 0){
+            if (i == '0'){
               dataList.empty();
               top = resl;
               $('#searchid').text(resl);
