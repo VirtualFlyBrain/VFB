0142855165c9308455a0668fcc78e218389384df
Robert Court <robbie.court@gmail.com>
2015-09-03 14:24:15 +0100

    doubling thumbnail size

--- javascript/vfb/stackManager.js
+++ javascript/vfb/stackManager.js
@@ -988,7 +988,7 @@ function thumbnailHTMLForId(id) {
   var url = "";
   var html = '<img class="lazy" data-original="URL" onclick="';
   html += "openFullDetails('" + id + "');";
-  html += '" alt="click to see full details" style="display: inline;" width="' + (($(window).width()/12.0)*7.0)*0.1 + 'px" />';
+  html += '" alt="click to see full details" style="display: inline;" width="' + (($(window).width()/12.0)*7.0)*0.2 + 'px" />';
   if (id.indexOf("VFBi_")>-1){
     url = "/data/VFB/i/" + id.substr(5,4) + "/" + id.substr(9,4) + "/thumbnail.png";
     return html.replace('"URL"', '"' + url + '"');
