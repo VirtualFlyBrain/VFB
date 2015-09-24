8491fa47810fcc84731bf73c307d75df205044b1
Robert Court <robbie.court@gmail.com>
2015-09-24 08:49:26 +0100

    adding debug

--- javascript/vfb/wlziip.js
+++ javascript/vfb/wlziip.js
@@ -1460,7 +1460,7 @@ function loadRightMenuDisplayed() {
           // index:
           index = String(i);
           if (rowD === null || rowD[1] !== index || (rowD[0].indexOf('"nameFor') > -1 && layer.name) || (rowD[3].indexOf('"typeFor') > -1 && layer.type) || (rowD[4].indexOf('fxp=undefined')>-1)){
-            //console.log('Update for row ' + String(i) + ' - ' + rowD);
+            console.log('Update for row ' + String(i) + ' - ' + rowD[0]);
             if (layer.id.indexOf('_a')>-1){
               layer.name = cleanIdforExt(layer.id);
               layer.type = "Private User Data";
@@ -1533,7 +1533,7 @@ function loadRightMenuDisplayed() {
             // Controls:
             controls = createControlsBarHTML(layer.id);
             if (rowD[2] !== controls) {
-              //console.log('Update controls for row ' + String(i));
+              console.log('Update controls for row ' + String(i));
               $('#displayed').dataTable().fnUpdate(controls,i,2, false );
               //console.log('Updating controls for ' + index + ' in the displayed layers');
             }
