8a39a42fe7f3d3dce5afb28728f05243adcc0af6
Robert Court <robbie.court@gmail.com>
2015-11-27 19:22:42 +0000

    logic fix

--- javascript/vfb/stackManager.js
+++ javascript/vfb/stackManager.js
@@ -935,7 +935,7 @@ function openFullDetails(id) {
       }
       if (document.getElementById('details')){
         //jump('details');
-        if (history.state === null | history.state.id === undefined | history.state.id != cleanIdforExt(id)){
+        if (history.state === null | (history.state !== null && history.state.id === undefined) | (history.state !== null && history.state.id === undefined && history.state.id != cleanIdforExt(id))){
           history.pushState({"id": id}, $('#partName').text(), location.pathname+"?id="+cleanIdforExt(id));
         }
       }
