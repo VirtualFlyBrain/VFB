f80e35d3a9ee92926948368aec502f3a2792f5ed
Robert Court <robbie.court@gmail.com>
2015-12-03 21:58:28 +0000

    testing without destroy

--- javascript/vfb/stackManager.js
+++ javascript/vfb/stackManager.js
@@ -1643,28 +1643,29 @@ function updateSearchResults() {
         }
         searchresults=uniq_json(newresults);
         engine.add(searchresults);
-        $('#searchtext').typeahead('destroy');
-        $('#searchtext').typeahead({
-          hint: true,
-          minLength: 1
-        }, {
-          display: 'syn',
-          limit: 20,
-          source: engine.ttAdapter(),
-          templates: {
-              empty: 'No matches yet...',
-              suggestion: function ( data ) {
-                  if (data.syn == data.name){
-                    return '<p onclick="clickSearchResult(' + "'" + data.id + "'" + ');"><b>' +  data.syn.replace($('#searchtext').val(),'<u>' + $('#searchtext').val() + '</u>') + "</b> <small>[<i>" + data.id.replace($('#searchtext').val(),'<u>' + $('#searchtext').val() + '</u>') + '</i>]</small></p>';
-                  }else if(data.syn == data.id){
-                    return '<p onclick="clickSearchResult(' + "'" + data.id + "'" + ');"><b>' +  data.syn.replace($('#searchtext').val(),'<u>' + $('#searchtext').val() + '</u>') + "</b> <small>(" + data.name.replace($('#searchtext').val(),'<u>' + $('#searchtext').val() + '</u>') + ')</small></p>';
-                  }else{
-                    return '<p onclick="clickSearchResult(' + "'" + data.id + "'" + ');"><b>' +  data.syn.replace($('#searchtext').val(),'<u>' + $('#searchtext').val() + '</u>') + "</b> <small>("+data.name.replace($('#searchtext').val(),'<u>' + $('#searchtext').val() + '</u>') + ") [<i>" + data.id.replace($('#searchtext').val(),'<u>' + $('#searchtext').val() + '</u>') + '</i>]</small></p>';
-                  }
-              }
-          }
-        });
-        $('#searchtext').focus();
+
+        // $('#searchtext').typeahead('destroy');
+        // $('#searchtext').typeahead({
+        //   hint: true,
+        //   minLength: 1
+        // }, {
+        //   display: 'syn',
+        //   limit: 20,
+        //   source: engine.ttAdapter(),
+        //   templates: {
+        //       empty: 'No matches yet...',
+        //       suggestion: function ( data ) {
+        //           if (data.syn == data.name){
+        //             return '<p onclick="clickSearchResult(' + "'" + data.id + "'" + ');"><b>' +  data.syn.replace($('#searchtext').val(),'<u>' + $('#searchtext').val() + '</u>') + "</b> <small>[<i>" + data.id.replace($('#searchtext').val(),'<u>' + $('#searchtext').val() + '</u>') + '</i>]</small></p>';
+        //           }else if(data.syn == data.id){
+        //             return '<p onclick="clickSearchResult(' + "'" + data.id + "'" + ');"><b>' +  data.syn.replace($('#searchtext').val(),'<u>' + $('#searchtext').val() + '</u>') + "</b> <small>(" + data.name.replace($('#searchtext').val(),'<u>' + $('#searchtext').val() + '</u>') + ')</small></p>';
+        //           }else{
+        //             return '<p onclick="clickSearchResult(' + "'" + data.id + "'" + ');"><b>' +  data.syn.replace($('#searchtext').val(),'<u>' + $('#searchtext').val() + '</u>') + "</b> <small>("+data.name.replace($('#searchtext').val(),'<u>' + $('#searchtext').val() + '</u>') + ") [<i>" + data.id.replace($('#searchtext').val(),'<u>' + $('#searchtext').val() + '</u>') + '</i>]</small></p>';
+        //           }
+        //       }
+        //   }
+        // });
+        // $('#searchtext').focus();
       });
     }
   }else{
