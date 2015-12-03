eb9debada547c9c1ed588018163b103a7c4921a7
Robert Court <robbie.court@gmail.com>
2015-12-03 17:56:30 +0000

    replacing tokeniser

--- javascript/vfb/stackManager.js
+++ javascript/vfb/stackManager.js
@@ -16,8 +16,8 @@ var searchresults = [{syn:'optic lobe',id:'FBbt_00003701',name:'optic lobe'},
       {syn:'AL2 clone',id:'FBbt_00110418',name:'adult fruitless aDT-c lineage clone'},
       {syn:'C1 tract',id:'FBbt_00005905',name:'C1 fascicle'}];
 var engine = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.obj.nonword("syn").split('_'),
-  queryTokenizer: Bloodhound.tokenizers.nonword,
+  datumTokenizer: function(d) { return Bloodhound.tokenizers.nonword(d.syn.replace('_',' ')); },
+  queryTokenizer: function(d) { return Bloodhound.tokenizers.nonword(d.replace('_',' ')); },
   local:searchresults,
   limit:20,
   sorter:function(a, b) {
