ad03a37e3be7e3bfede285e53c01262b133f433a
Rob Court <robbie.court@gmail.com>
2016-07-22 19:59:41 +0100

    adding template to opening links

--- javascript/vfb/stackManager.js
+++ javascript/vfb/stackManager.js
@@ -4751,7 +4751,7 @@ function genExamples(rootID, htmlid) {
         accepts: {json: "application/json"},
         dataType: "json",
         data: {
-            "query": 'MATCH (n { short_form: "' + rootID + '" } )<-[:SUBCLASSOF|INSTANCEOF*]-(i:Individual) RETURN i.short_form as exId, i.label as exName',
+            "query": 'MATCH (n { short_form: "' + rootID + '" } )<-[:SUBCLASSOF|INSTANCEOF*]-(i:Individual)-[:Related { label : 'depicts' } ]-(j:Individual)-[:Related { label : 'has_signal_channel' } ]-(k:Individual)-[:Related { label: 'has_background_channel' } ]-(m:Individual) RETURN i.short_form as exId, i.label as exName, m.short_form as tempId, m.label as tempName',
             "params": {}
         },
         type: "POST",
@@ -4780,13 +4780,13 @@ function genExamples(rootID, htmlid) {
                         html += ' active';
                     }
                     html += '">';
-                    html += thumbnailHTMLForId(item[0]).replace(' thumb_sm','').replace('openFullDetails','addToStackData').replace('click to see full details',item[1]+' ('+item[0]+')').replace('/>','style="max-width: 350px; min-width: 200px; min-height: 200px; cursor: pointer; display: block; margin: 0px auto; padding-bottom: 25px; padding-top: 10px;" />');
+                    html += thumbnailHTMLForId(item[0]).replace(' thumb_sm','').replace('openFullDetails','addToStackData').replace('click to see full details',item[1]+' ('+item[0]+')').replace('/>','style="max-width: 350px; min-width: 200px; min-height: 200px; cursor: pointer; display: block; margin: 0px auto; padding-bottom: 25px; padding-top: 10px;" />').replace(cleanIdforInt(item[0]),cleanIdforInt(item[2] + "," + cleanIdforInt(item[0])));
                     html += '<div class="carousel-caption" style="bottom:-33px;opacity:0.3;" title="open ' + item[1] + ' in viewer" onclick="openFullDetails(\'' + item[0] + '\');addToStackData(\'' + item[0] + '\');">';
                     html += '<b>' + item[1] + '</b><br>';
                     html += '<span class="small">' + item[0] + '</span>';
                     html += '</div></div>';
                 }
-                index[i] = item[0];
+                index[i] = cleanIdforInt(item[2] + "," + item[0];
                 i++;
             });
 
