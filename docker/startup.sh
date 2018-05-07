63ef1058e00634a531a9679d66f99ed16b44246f
Rob Court <robbie.court@gmail.com>
2018-05-07 16:42:33 +0100

    install on load

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+cd /opt/tomcat/webapps/
+rm -rf ROOT
+git clone https://github.com/VirtualFlyBrain/VFB.git -b docker-server ROOT
 cd /opt/tomcat/webapps/ROOT/
-deploy/update.sh
+deploy/decompress.sh
+cd /opt/tomcat/webapps/ROOT/
+deploy/Deploy-Main-Server.sh
 
 tail -F /opt/tomcat/logs/ontServer.log &
 tail -F /opt/tomcat/logs/catalina.out &
