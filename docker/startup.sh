46057433808745b74fdda7dbb8316273efe3960a
Rob Court <robbie.court@gmail.com>
2018-05-09 17:21:06 +0100

    fixing order

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-echo 'Starting Server...' >> /opt/tomcat/webapps/ROOT/logs/ontServer.log
-chmod -R 777 /opt/tomcat/webapps/ROOT/logs/ontServer.log
 
 cd /opt/tomcat/webapps/
 rm -rf ROOT
 git clone https://github.com/VirtualFlyBrain/VFB.git -b docker-server ROOT
 cd /opt/tomcat/webapps/ROOT/
 sed -i 's/localhost/database/g' /opt/tomcat/webapps/ROOT/src/db.xml
+
 deploy/decompress.sh
+
+echo 'Starting Server...' >> /opt/tomcat/webapps/ROOT/logs/ontServer.log
+chmod -R 777 /opt/tomcat/webapps/ROOT/logs/ontServer.log
+
 cd /opt/tomcat/webapps/ROOT/
 deploy/Deploy-Main-Server.sh
 
