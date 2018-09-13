6c89cd2019af833e0a5e34036cab69bb491c4fc6
Rob Court <robbie.court@gmail.com>
2018-09-13 16:04:25 +0100

    basic tree code

---
+++ javascript/vfb/anatomyTree.js
@@ -0,0 +1,159 @@
+		var nodeIndex = [];
+    var template = "VFB_00017894";
+    var AUTHORIZATION = "Basic " + btoa("neo4j:vfb");
+    /**
+     * Uses JQuery to post an ajax request on Neo4j REST API
+     */
+    function restPost(data) {
+        var strData = JSON.stringify(data);
+        return $.ajax({
+            type: "POST",
+            beforeSend: function (request) {
+                if (AUTHORIZATION != undefined) {
+                    request.setRequestHeader("Authorization", AUTHORIZATION);
+                }
+            },
+            url: "http://pdb.virtualflybrain.org/db/data/transaction/commit",
+            contentType: "application/json",
+            data: strData
+        });
+    }
+    /**
+     * Function to call to display a new graph.
+     */
+    function displayGraph() {
+        // Create the authorization header for the ajax request.
+        AUTHORIZATION = "Basic " + btoa($("#userinput").val() + ":" + $("#passinput").val());
+        // Show loading elements.
+        $("#loading").show();
+        $("#loadingBar").show();
+        document.getElementById('text').innerHTML = '0%';
+        document.getElementById('bar').style.width = '0';
+        document.getElementById('loadingBar').style.opacity = 1;
+        var start = template;
+        // Post Cypher query to return node and relations and return results as graph.
+        restPost({
+            "statements": [
+                {
+                    "statement": "MATCH (t:Individual { short_form : '" + start + "'})<-[:depicts]-(tc:Individual)<-[ie:in_register_with]-(c:Individual)-[:depicts]->(image:Individual)-[:INSTANCEOF]->(ac:Class) WHERE has(ie.index) WITH  COLLECT (ac.short_form) as tree_nodes, COLLECT (DISTINCT{ image: image.short_form, anat_ind: image.short_form, type: ac.short_form}) AS domain_map MATCH p=allShortestPaths((root:Class { label : 'adult brain'})<-[:SUBCLASSOF|part_of*..]-(anat:Class)) WHERE anat.short_form IN tree_nodes RETURN p, domain_map",
+                    "resultDataContents": ["graph"]
+                }
+            ]
+        }).done(function (data) {
+            $("#loading").hide();
+            // Parse results and convert it to vis.js compatible data.
+            var graphData = parseGraphResultData(data);
+            var nodes = convertNodes(graphData.nodes);
+            var edges = convertEdges(graphData.edges);
+            var visData = {
+                nodes: nodes,
+                edges: edges
+            };
+            displayVisJsData(visData);
+        });
+    }
+    
+    function displayVisJsData(data) {
+        var container = document.getElementById('vis');
+        var options = {
+            nodes: {
+                shape: 'icon',
+                icon: {
+                  face: 'FontAwesome',
+                  code: '\uf05a',
+                  size: 50,
+                  color: '#aa00ff'
+                },
+                mass: 1
+            },
+            layout: {
+            		hierarchical: {
+                		enabled: true,
+                    direction: 'LR',
+                    levelSeparation: 350                    
+                }
+            }
+        };
+        // initialize the network!
+        var network = new vis.Network(container, data, options);
+        network.on("stabilizationProgress", function (params) {
+            var maxWidth = 496;
+            var minWidth = 20;
+            var widthFactor = params.iterations / params.total;
+            var width = Math.max(minWidth, maxWidth * widthFactor);
+            document.getElementById('bar').style.width = width + 'px';
+            document.getElementById('text').innerHTML = Math.round(widthFactor * 100) + '%';
+        });
+        network.once("stabilizationIterationsDone", function () {
+            document.getElementById('text').innerHTML = '100%';
+            document.getElementById('bar').style.width = '496px';
+            document.getElementById('loadingBar').style.opacity = 0;
+            // really clean the dom element
+            setTimeout(function () {
+                $("#loadingBar").hide();
+            }, 500);
+        });
+        network.on("click", function (params) {
+          params.event = "[original event]";
+          console.log(nodeIndex[params.nodes[0]]);
+      	});
+    }
+    
+    function parseGraphResultData(data) {
+        var nodes = {}, edges = {};
+        data.results[0].data.forEach(function (row) {
+            row.graph.nodes.forEach(function (n) {
+                if (!nodes.hasOwnProperty(n.id)) {
+                    nodes[n.id] = n;
+                }
+            });
+            row.graph.relationships.forEach(function (r) {
+                if (!edges.hasOwnProperty(r.id)) {
+                    edges[r.id] = r;
+                }
+            });
+        });
+        var nodesArray = [], edgesArray = [];
+        for (var p in nodes) {
+            if (nodes.hasOwnProperty(p)) {
+                nodesArray.push(nodes[p]);
+            }
+        }
+        for (var q in edges) {
+            if (edges.hasOwnProperty(q)) {
+                edgesArray.push(edges[q])
+            }
+        }
+        return {nodes: nodesArray, edges: edgesArray};
+    }
+    
+    function convertNodes(nodes) {
+        var convertedNodes = [];
+        nodes.forEach(function (node) {
+            var nodeLabel = node.properties['short_form'];
+            var displayedLabel = node.properties['label'];
+            convertedNodes.push({
+                id: node.id,
+                label: displayedLabel,
+                group: nodeLabel
+            })
+            nodeIndex[node.id]=nodeLabel;
+        });
+        return convertedNodes;
+    }
+    
+    function convertEdges(edges) {
+        var convertedEdges = [];
+        edges.forEach(function (edge) {
+        		var relatType = edge.type.replace("_"," ");
+            if (relatType.indexOf("Related")>-1){
+            	relatType = edge.properties['label'].replace("_"," ");
+            }
+            convertedEdges.push({
+                from: edge.startNode,
+                to: edge.endNode,
+                label: relatType
+            })
+        });
+        return convertedEdges;
+    }
