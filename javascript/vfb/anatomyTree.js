0f4339dbba91879b2d2858d570728b9e9d3497b1
Rob Court <rcourt@ed.ac.uk>
2018-09-21 10:50:12 +0100

    Revert "daming"
    This reverts commit 649b9f0fc1b05c5b8d55a5f0e81ca609ce6bf498.

--- javascript/vfb/anatomyTree.js
+++ javascript/vfb/anatomyTree.js
@@ -121,10 +121,10 @@
                 enabled: true,
                 hierarchicalRepulsion: {
                   centralGravity: 0.3,
-                  springLength: 150,
+                  springLength: 200,
                   springConstant: 0.4,
                   nodeDistance: 250,
-                  damping: 0.2
+                  damping: 0.08
                 },
                 maxVelocity: 500,
                 minVelocity: 0.1,
