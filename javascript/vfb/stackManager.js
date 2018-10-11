1321346e23cb8e40a8be5d8c2c23a1ea8808c9b1
Rob Court <robbie.court@gmail.com>
2018-10-11 13:11:53 +0100

    converting internal refs to official id

--- javascript/vfb/stackManager.js
+++ javascript/vfb/stackManager.js
@@ -717,6 +717,10 @@ function cleanIdforExt(id) {
         id = id.replace(":", "_");
         id = id.toLowerCase().replace("vfb", "VFB").replace('fb', 'FB');
         id = id.replace('VFBi_', 'VFB_');
+        if (id.indexOf('VFBt_')>-1 && id.length == 8){
+            id=id+'00000';    
+        }
+        id = id.replace('VFBt_00100000','VFB_00017894').replace('VFBt_00200000','VFB_00030786').replace('VFBt_00300000','VFB_00049000').replace('VFBt_00400000','VFB_00100000').replace('VFBt_00500000','VFB_00050000')
         id = id.replace('VFBexp_FB','FB').replace('VFBexpFB','FB');
         if (id.indexOf('fbbt') > -1) {
             id = id.substr(0, id.indexOf('fbbt'));
@@ -730,7 +734,11 @@ function cleanIdforInt(id) {
     if (id) {
         id = id.replace(":", "_");
         id = id.toLowerCase().replace("vfb", "VFB").replace('fb', 'FB');
+        id = id.replace('VFB_00017894','VFBt_001').replace('VFB_00030786','VFBt_002').replace('VFB_00049000','VFBt_003').replace('VFB_00100000','VFBt_004').replace('VFB_00050000','VFBt_005')
         id = id.replace('VFB_', 'VFBi_');
+        if (id.indexOf('VFBt_')>-1 && id.length > 8){
+            id=id.replace('00000','');
+        }
         if (id.indexOf('fbbt') > -1) {
             id = id.substr(0, id.indexOf('fbbt'));
         }
