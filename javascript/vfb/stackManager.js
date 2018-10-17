5592e4bc2df7c5653d02ddd4ada6712e30903d95
Rob Court <robbie.court@gmail.com>
2018-10-17 16:30:36 +0100

    fix for adding outside stack viewer

--- javascript/vfb/stackManager.js
+++ javascript/vfb/stackManager.js
@@ -1600,8 +1600,10 @@ function addToStackData(ids, showDetails) {
                             }
                             parent.$("body").data("disp", "scale");
                             updateStackData();
-                            loadReferenceMeta(id);
-                            $('#searchtext').blur();
+                            if (typeof loadReferenceMeta !== "undefined"){
+                                loadReferenceMeta(id);
+                                $('#searchtext').blur();
+                            }
                             //try {history.pushState( {}, parent.$("body").data("meta").name, '/site/stacks/index.htm?add='+id );}catch (ignore){}
                             //  if (window.location.pathname == "/site/stacks/index.htm"){
                             //    location.href=location.href.replace(location.hash,"").replace('#','');
