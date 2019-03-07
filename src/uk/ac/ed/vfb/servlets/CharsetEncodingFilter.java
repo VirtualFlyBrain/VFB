74c10151c94518102cf5d7d07928bdcca8e86a4e
Rob Court <rcourt@ed.ac.uk>
2019-03-07 12:55:56 +0000

    adding debug

--- src/uk/ac/ed/vfb/servlets/CharsetEncodingFilter.java
+++ src/uk/ac/ed/vfb/servlets/CharsetEncodingFilter.java
@@ -33,7 +33,7 @@ public class CharsetEncodingFilter implements Filter {
 	    LOG.debug("servletResponse CharEncoding: " + servletResponse.getCharacterEncoding());
             filterChain.doFilter(servletRequest, servletResponse);
         } catch (Exception ex) {
-			LOG.error("Exception running CharsetEncodingFilter: request:" + servletRequest.toString() + " response:" + servletResponse.toString());
+			LOG.error("Exception running CharsetEncodingFilter: " + servletRequest.getMethod() + " " + servletRequest.getRequestURI());
 		    ex.printStackTrace();
 		}
         
