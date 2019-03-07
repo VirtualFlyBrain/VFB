e357c8dd03c3f23aba4334176b01f87303d88f9c
Rob Court <rcourt@ed.ac.uk>
2019-03-07 08:35:41 +0000

    testing ISO-8859-1

--- src/uk/ac/ed/vfb/servlets/CharsetEncodingFilter.java
+++ src/uk/ac/ed/vfb/servlets/CharsetEncodingFilter.java
@@ -25,7 +25,7 @@ public class CharsetEncodingFilter implements Filter {
 	    LOG.debug("servletRequest CharEncoding: " + servletRequest.getCharacterEncoding());
             LOG.debug("servletRepsonse Content Type: " + servletResponse.getContentType());
 	    LOG.debug("servletResponse CharEncoding: " + servletResponse.getCharacterEncoding());
-            servletRequest.setCharacterEncoding("UTF-8");
+            servletRequest.setCharacterEncoding("ISO-8859-1");
             servletResponse.setContentType("text/html;charset=UTF-8");
 	    LOG.debug("servletRequest Content Type: " + servletRequest.getContentType());
 	    LOG.debug("servletRequest CharEncoding: " + servletRequest.getCharacterEncoding());
