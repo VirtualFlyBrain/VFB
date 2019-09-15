3f82a453c4730617fb6866fb61b3edc654a4d6f4
Rob Court <rcourt@ed.ac.uk>
2019-09-15 19:57:18 +0100

    Revert "not needed for supertype"
    This reverts commit acd5164c5af4b80dc01bd66aea6f40acc7c39254.

--- src/uk/ac/ed/vfb/annotation/web/ProcessRunner.java
+++ src/uk/ac/ed/vfb/annotation/web/ProcessRunner.java
@@ -58,6 +58,7 @@ public class ProcessRunner extends Thread {
 	}
 
 	@SuppressWarnings("deprecation")
+	@Override
 	public void destroy() {
 		//LOG.debug("Killing current process and closing the thread down");
 		Utils.runCommand(ProcessRunner.KILL_COMMAND + this.PID);
