641ef043121bb2849c36131be02ce2d18d29f853
Rob Court <rcourt@ed.ac.uk>
2019-10-16 13:00:23 +0100

    fixing links

--- deploy/start-docker-server-Ont-Server.sh
+++ deploy/start-docker-server-Ont-Server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export VFBclasses=/opt/tomcat/webapps/VFB/WEB-INF/classes
+export VFBclasses=/disk/data/tomcat/fly/webapps/vfb/WEB-INF/classes
 export branch=docker-server
 
 #start / force restart of ontology server 
