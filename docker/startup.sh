89bb16f80c31b6edf1ec2b50d7567ebbf6978f23
Rob Court <rcourt@ed.ac.uk>
2019-10-29 19:41:52 +0000

    Update startup.sh

--- docker/startup.sh
+++ docker/startup.sh
@@ -19,4 +19,5 @@ tail -F /usr/local/tomcat/webapps/vfb/logs/ontServer.log
 #sed -i 's|<Connector port="8009"|<Connector port="80" maxHttpHeaderSize="8192" maxThreads="150" minSpareThreads="25" maxSpareThreads="75" proxyName="vfb" proxyPort="80" enableLookups="false" redirectPort="8443" acceptCount="100" connectionTimeout="20000" disableUploadTimeout="true" useURIValidationHack="false" />\n<Connector port="8009"|g' server.xml
 #sed -i 's|</Host>|</Host>\n<Host name="vfb" debug="1" appBase="webapps" unpackWARS="false" autoDeploy="false" deployOnStartup="false">\n           <Context path="" reloadable="true"\n               docBase="/opt/tomcat/webapps/VFB/">\n           </Context>\n     </Host>|g' server.xml
 
-catalina.sh run
+/usr/local/tomcat/bin/catalina.sh run
+
