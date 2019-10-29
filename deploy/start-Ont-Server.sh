e72d27f3e77524f75ff10cc9d48ccf7430f99bf6
Robbie Court <rcourt@mustard.inf.ed.ac.uk>
2019-10-29 17:54:37 +0000

    working on mustard

--- deploy/start-Ont-Server.sh
+++ deploy/start-Ont-Server.sh
@@ -6,7 +6,6 @@ then
   psCmd=`ps ax |grep 'client_server.Server'  | grep ${path} | awk '{print $1}'`
   if [ $psCmd ]
   then
-    service tomcat stop
     echo "Ontology server is already running"
     echo "Killing "$psCmd
 	  kill -9 $psCmd
@@ -19,7 +18,7 @@ then
   cd $path
   echo "--RESTART--" >> ../../logs/ontServer.log
   #Note: path variable usage is so the run can be distinguised for forced shutdown.
-  daemonize -u tomcat -a -c $path -e $path/../../logs/ontServer.log -o $path/../../logs/ontServer.log -p $path/../../logs/ontServer-pid.log -E BUILD_ID=dontKillMe /usr/bin/java -classpath ../lib/postgresql-9.0-801.jdbc3.jar:../lib/commons-pool.jar:../lib/commons-dbcp.jar:${path}/../lib/servlet-api.jar:../lib/spring.jar::../lib/commons-logging.jar:../lib/log4j.jar:../lib/Brain-1.5.1-jar-with-dependencies.jar:../lib/elk-owlapi.jar:../lib/owltools-oort-all.jar:. -Djava.rmi.server.codebase=file:$path -Dlog4j.configuration=./log4j.properties uk.ac.ed.vfb.dao.client_server.Server
+  daemonize -a -c $path -e $path/../../logs/ontServer.log -o $path/../../logs/ontServer.log -p $path/../../logs/ontServer-pid.log -E BUILD_ID=dontKillMe /usr/bin/java -classpath ../lib/postgresql-9.0-801.jdbc3.jar:../lib/commons-pool.jar:../lib/commons-dbcp.jar:${path}/../lib/servlet-api.jar:../lib/spring.jar::../lib/commons-logging.jar:../lib/log4j.jar:../lib/Brain-1.5.1-jar-with-dependencies.jar:../lib/elk-owlapi.jar:../lib/owltools-oort-all.jar:. -Djava.rmi.server.codebase=file:$path -Dlog4j.configuration=./log4j.properties uk.ac.ed.vfb.dao.client_server.Server
   # nohup nice /usr/lib/jvm/java-1.7.0-sun/bin/java -classpath ../lib/postgresql-9.0-801.jdbc3.jar:../lib/commons-pool.jar:../lib/commons-dbcp.jar:${path}/../lib/servlet-api.jar:../lib/spring.jar::../lib/commons-logging.jar:../lib/log4j.jar:../lib/Brain-1.5.1-jar-with-dependencies.jar:../lib/elk-owlapi.jar:../lib/owltools-oort-all.jar:. -Djava.rmi.server.codebase=file:$path -Dlog4j.configuration=./log4j.properties uk.ac.ed.vfb.dao.client_server.Server > ../../logs/ontServer.log 2>&1 &
 
 echo 'waiting for the ontology server to initialise...'
@@ -30,7 +29,6 @@ until [ `cat ../../logs/ontServer.log | grep "Wainting for a connection" | wc -l
 done
 let MAX_WAIT=(24-MAX_WAIT)*5
 echo Ontology server restart took ${MAX_WAIT} Seconds
-sudo /bin/systemctl restart tomcat
 else
   echo "Must be run from specific server script"
 fi
---
+++ filters/Local-docker-server-Smudge.sed
@@ -0,0 +1,7 @@
+s/www.virtualflybrain.org/www.virtualflybrain.org/
+s/>vfb</>vfb</
+s/-vfb-/-vfb-/
+/webapps/s/vfb/vfb/
+s/WEBAPP_NAME\/vfb\//WEBAPP_NAME\/vfb\//
+s/UA-18509775-2\/UA-18509775-2/UA-18509775-2\/UA-18509775-2/
+s/ont_port=5555\/ont_port=5555/ont_port=5555\/ont_port=5555/g
